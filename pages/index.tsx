68930530af79616013035d872d0eafae13fc4eab
Pratheek Senevirathne <pratheeksenevirathne@gmail.com>
2023-12-05 19:17:27 +0530

    Fix header animation on smaller devices

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,12 +14,12 @@ import { useEffect, useState } from 'react';
 const openSans = Open_Sans({ subsets: ['latin'] });
 
 export default function Home() {
-  const [animationClass, setAnimationClass] = useState('h-0');
+  const [animationClass, setAnimationClass] = useState('max-h-0');
   const [animationClass2, setAnimationClass2] = useState('opacity-0');
 
   useEffect(() => {
     setTimeout(() => {
-      setAnimationClass('h-56');
+      setAnimationClass('max-h-96');
     }, 200);
   }, []);
 
@@ -40,7 +40,7 @@ export default function Home() {
           className='absolute top-0 left-0 -z-20 opacity-80'
           alt='Background Image'
           src={backgroundImage}
-          style={{ objectFit: 'cover', objectPosition: 'center', filter: 'blur(1px)' }}
+          style={{ objectFit: 'cover', objectPosition: 'center', filter: 'blur(3px)' }}
           fill
           priority
           placeholder='blur'
@@ -49,7 +49,7 @@ export default function Home() {
         <div className='absolute -z-10 top-0 left-0 bg-gradient-to-t from-black/75 to-black/20 h-full w-full'></div>
 
         <div className='relative min-h-screen'>
-          <div className='absolute top-1/2 left-0 w-full -translate-y-1/2'>
+          <div className='absolute top-1/2 left-0 w-full -translate-y-1/2 p-5'>
             <div className='flex justify-center border-2 border-white rounded-full w-fit mx-auto'>
               <div className='relative'>
                 <Image
@@ -61,21 +61,21 @@ export default function Home() {
                   placeholder='blur'
                   priority
                 />
-                <div className='absolute top-0 left-0 h-[120px] w-[120px] bg-gradient-to-b from-black/20 to-transparent rounded-full'></div>
+                <div className='absolute top-0 left-0 h-[120px] w-[120px] bg-gradient-to-b from-black/30 to-transparent rounded-full'></div>
               </div>
             </div>
 
             <div className='h-12 border border-white w-0 mx-auto'></div>
 
             <div
-              className={`border-t-2 border-b-2 border-white max-w-4xl mx-auto transition-all duration-500 ${animationClass}`}
+              className={`border-t-2 border-b-2 border-white max-w-4xl mx-auto transition-all duration-1000 ${animationClass}`}
             >
-              <div className={`transition-opacity duration-200 ${animationClass2} py-12`}>
+              <div className={`py-12 transition-opacity duration-300 ${animationClass2}`}>
                 <h1 className='text-4xl font-medium text-center mb-4'>Pratheek Senevirathne</h1>
 
                 <h2 className='text-xl text-center'>
                   BSc. (Hons) Computer Science (Reading) | Entrepreneur | Robotics and Tech
-                  Enthusiast | 3D Designer | Innovator
+                  Enthusiast | <br /> 3D Designer | Innovator
                 </h2>
               </div>
             </div>
@@ -93,7 +93,7 @@ export default function Home() {
           </div>
         </div>
       </header>
-      <main className={`px-5 flex min-h-screen flex-col items-center justify-between`}></main>
+      {/* <main className={`px-5 flex min-h-screen flex-col items-center justify-between`}></main> */}
     </div>
   );
 }
